d92ca6c58641bcd25b0c6e5c532333dbbe2cb77d
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2021-04-30 22:30:07 +0200

    Portfolio 8
    Portfolio 8

---
+++ Assets/Portfolio7/NavController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavController : MonoBehaviour
+{
+
+    UnityEngine.AI.NavMeshAgent agent;
+    public Animator animator;
+    public string parametrBoolean;
+    bool b = false;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        b = !b;
+        animator.SetBool(parametrBoolean, b);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            
+            animator.SetBool(parametrBoolean, false);
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+
+            }
+
+         
+        }
+
+        animator.SetBool(parametrBoolean, true);
+    }
+}
